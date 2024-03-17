3c1953451d2fa03be9e9aecb4bbcec17bd6a1172
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-17 11:40:11 +0100

    Initial Commit

---
+++ New Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New Rock Paper Scissors
+//
+//  Created by Carlos Román Alcaide on 17/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New Rock Paper Scissors/New_Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Rock_Paper_ScissorsApp.swift
+//  New Rock Paper Scissors
+//
+//  Created by Carlos Román Alcaide on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct New_Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
